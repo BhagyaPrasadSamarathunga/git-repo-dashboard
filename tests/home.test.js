90f7b5bb17dff836e8acf665b68c39bc2730b35d
Bhagya <bhagyasamarathunga@gmail.com>
2023-04-11 02:46:40 +0200

    Add unit tests

---
+++ tests/home.test.js
@@ -0,0 +1,9 @@
+const isPopularRepo = require('../src/js/home.js');
+
+test('Returns the repo is not popular', function () {
+	expect(isPopularRepo(1,2)).toBe('Not Popular');
+});
+
+test('Returns the repo is popular', function () {
+	expect(isPopularRepo(100,300)).toBe('Popular');
+});
